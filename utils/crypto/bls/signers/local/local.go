71c1da03222552df98e663ecf597b558bbcc8dc5
Richard Pringle <richard.pringle@avalabs.org>
2024-12-13 14:05:32 -0500

    Move local-signer to its own package

--- utils/crypto/bls/signers/local/local.go
+++ utils/crypto/bls/signers/local/local.go
@@ -1,39 +1,28 @@
 // Copyright (C) 2019-2024, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-package bls
+package local
 
 import (
 	"crypto/rand"
 	"errors"
 	"runtime"
 
+	"github.com/ava-labs/avalanchego/utils/crypto/bls" // Import the parent package
 	blst "github.com/supranational/blst/bindings/go"
 )
 
 const SecretKeyLen = blst.BLST_SCALAR_BYTES
 
 var (
-	errFailedSecretKeyDeserialize = errors.New("couldn't deserialize secret key")
-
-	// The ciphersuite is more commonly known as G2ProofOfPossession.
-	// There are two digests to ensure that message space for normal
-	// signatures and the proof of possession are distinct.
-	ciphersuiteSignature                = []byte("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_")
-	ciphersuiteProofOfPossession        = []byte("BLS_POP_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_")
-	_                            Signer = (*LocalSigner)(nil)
+	errFailedSecretKeyDeserialize            = errors.New("couldn't deserialize secret key")
+	_                             bls.Signer = (*LocalSigner)(nil) // Use the Signer interface from the parent package
 )
 
-type SecretKey = blst.SecretKey
-
-type Signer interface {
-	PublicKey() *PublicKey
-	Sign(msg []byte) *Signature
-	SignProofOfPossession(msg []byte) *Signature
-}
+type secretKey = blst.SecretKey
 
 type LocalSigner struct {
-	sk *SecretKey
+	sk *secretKey
 }
 
 // NewSecretKey generates a new secret key from the local source of
@@ -58,11 +47,11 @@ func (s *LocalSigner) ToBytes() []byte {
 // SecretKeyFromBytes parses the big-endian format of the secret key into a
 // secret key.
 func SecretKeyFromBytes(skBytes []byte) (*LocalSigner, error) {
-	sk := new(SecretKey).Deserialize(skBytes)
+	sk := new(secretKey).Deserialize(skBytes)
 	if sk == nil {
 		return nil, errFailedSecretKeyDeserialize
 	}
-	runtime.SetFinalizer(sk, func(sk *SecretKey) {
+	runtime.SetFinalizer(sk, func(sk *secretKey) {
 		sk.Zeroize()
 	})
 	return &LocalSigner{sk: sk}, nil
@@ -70,16 +59,16 @@ func SecretKeyFromBytes(skBytes []byte) (*LocalSigner, error) {
 
 // PublicKey returns the public key that corresponds to this secret
 // key.
-func (s *LocalSigner) PublicKey() *PublicKey {
-	return new(PublicKey).From(s.sk)
+func (s *LocalSigner) PublicKey() *bls.PublicKey {
+	return new(bls.PublicKey).From(s.sk)
 }
 
 // Sign [msg] to authorize this message
-func (s *LocalSigner) Sign(msg []byte) *Signature {
-	return new(Signature).Sign(s.sk, msg, ciphersuiteSignature)
+func (s *LocalSigner) Sign(msg []byte) *bls.Signature {
+	return new(bls.Signature).Sign(s.sk, msg, bls.CiphersuiteSignature)
 }
 
 // Sign [msg] to prove the ownership
-func (s *LocalSigner) SignProofOfPossession(msg []byte) *Signature {
-	return new(Signature).Sign(s.sk, msg, ciphersuiteProofOfPossession)
+func (s *LocalSigner) SignProofOfPossession(msg []byte) *bls.Signature {
+	return new(bls.Signature).Sign(s.sk, msg, bls.CiphersuiteProofOfPossession)
 }
